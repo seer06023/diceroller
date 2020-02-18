be7264b3ac6d50bff96835c9f44342f40f333050
erseer <seer6023@gmail.com>
2020-02-18 22:53:20 +0300

    random generator

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.makeText
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +20,16 @@ class MainActivity : AppCompatActivity() {
         rollbutton.text = "Let's Roll"
 
         rollbutton.setOnClickListener {
-            makeText(this, "Button Pressed", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
     }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt: Int =  Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
+    }
+
+
 }
